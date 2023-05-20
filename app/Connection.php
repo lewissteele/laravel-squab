c99742419de258644a736f808338622d898052fd
Lewis Steele <lewissteele254@gmail.com>
2023-05-20 20:09:34 +0100

    Add columns to fillable

--- app/Connection.php
+++ app/Connection.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Connection extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'host',
+        'password',
+        'username',
+    ];
 }
