64ad5e2cc2775c0f45446985ed4e12ec3bc31628
Lewis Steele <lewissteele254@gmail.com>
2023-05-21 11:47:37 +0100

    Test saving connection to db

--- tests/Feature/AddCommandTest.php
+++ tests/Feature/AddCommandTest.php
@@ -2,21 +2,30 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Console\Command;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AddCommandTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_saving_connection(): void
     {
-        $this->artisan('inspire')
-          // ->expectsOutput('')
-             ->assertExitCode(0);
+        $host = fake()->domainName();
+        $password = fake()->password();
+        $username = fake()->userName();
+
+        $this->artisan('add')
+            ->expectsQuestion('What is the host name?', $host)
+            ->expectsQuestion('What is your user name?', $username)
+            ->expectsQuestion('What is your password?', $password)
+            ->assertExitCode(Command::SUCCESS);
+
+        $this->assertDatabaseHas('connections', [
+            'host' => $host,
+            'password' => $password,
+            'username' => $username,
+        ]);
     }
 }
