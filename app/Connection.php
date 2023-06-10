c54daeaaeb12b1604e6f6fe4eedaf0022665b753
Lewis Steele <lewissteele254@gmail.com>
2023-06-10 13:26:24 +0100

    Add doc block for model columns

--- app/Connection.php
+++ app/Connection.php
@@ -5,6 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $host
+ * @property string $password
+ * @property string $password
+ */
 class Connection extends Model
 {
     use HasFactory;
